7c7412ab8161338eaf51e7d1585af24340793f01
Dominika <potterka@gmail.com>
2017-05-30 22:49:43 +0200

    Added MainMenu scene

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
